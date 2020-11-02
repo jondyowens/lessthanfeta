c8905d927dfb3211fa70d73fa3d54662b0c5859d
jondy <jondyowens@gmail.com>
2020-11-01 23:02:18 -0800

    Modified how subscriber emotes are stripped from emoji dump command. Moved emoji dump to separate function.

--- bot.py
+++ bot.py
@@ -14,6 +14,18 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+def remove_sub_emojis(all_emojis):
+    stripped_emojis = [i for i in all_emojis if i.managed == False]
+    return stripped_emojis
+
+async def emoji_dump(message):
+    emoji_dump = str()
+    stripped_emojis = remove_sub_emojis(client.emojis)
+    for emoji in stripped_emojis:
+            emoji_dump += str(emoji) + " "
+
+    await message.channel.send(emoji_dump)
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -24,11 +36,6 @@ async def on_message(message):
         await message.add_reaction(random.choice(list_of_cheese))
 
     if "!emojis" in message.content:
-        emoji_dump = str()
-        for emojis in client.emojis:
-            if not emojis.managed:
-                emoji_dump += str(emojis) + " "
-
-        await message.channel.send(emoji_dump)
+        await emoji_dump(message)
 
 client.run(TOKEN)
